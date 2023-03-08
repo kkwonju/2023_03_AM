67c07b764bc1891c42d93dfad93997552ff711ef
kkwonju <kkwo4140@gmail.com>
2023-03-08 17:45:44 +0900

    명령어 입력 실습

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,19 +1,44 @@
+//? : 저장되어 불러내기가 가능한 단계로 만들기
+//과제 단락 나눠 해결하는 연습부터, 하나씩 발전되는 식으로 코딩
+//command.length() == 0 : 입력 없음
+
 package com.KoreaIT.java.AM;
 
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		System.out.println("==프로그램 시작==");
-
+		
 		Scanner sc = new Scanner(System.in);
 
+		int num = 0;
 		while (true) {
 			System.out.print("명령어 > ");
 			String command = sc.nextLine();
 			
-			if(command.equals("exit")) {
+			if(command.equals("article list")){
+				if(num > 0) {
+					;
+				}
+				else {
+					System.out.println("게시글이 없습니다");
+				}
+			}
+			else if(command.equals("article write")) {
+				num++;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String content = sc.nextLine();
+				System.out.printf("%d번 글이 생성되었습니다\n", num);
+				
+			}
+			else if(command.equals("exit")) {
 				break;
+			}
+			else {
+				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
 
@@ -22,3 +47,4 @@ public class Main {
 		sc.close();
 	}
 }
+
---
+++ src/com/KoreaIT/java/AM/Main_2.java
@@ -0,0 +1,52 @@
+/* 강사님 코드
+package com.KoreaIT.java.AM;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+
+		Scanner sc = new Scanner(System.in);
+
+		int lastArticleId = 0;
+
+		while (true) {
+			System.out.print("명령어 > ");
+			String command = sc.nextLine();
+
+			if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (command.equals("exit")) {
+				break;
+			}
+
+			if (command.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+
+//				System.out.println(title + " / " + body);
+
+				System.out.printf("%d번글이 생성되었습니다\n", id);
+				lastArticleId++;
+
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
+			}
+		}
+
+		System.out.println("==프로그램 끝==");
+
+		sc.close();
+	}
+}
+
+*/
