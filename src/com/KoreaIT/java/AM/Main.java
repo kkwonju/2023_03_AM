6f3c41b4c08ea46de2ab3db4242577b344e636bf
kkwonju <kkwo4140@gmail.com>
2023-03-10 19:12:56 +0900

    게시물 찾는 기능, 회원가입 기능 과정

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -9,10 +9,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	/* articles : Article만 넣을 수 있는 List 객체 생성 */
 	static List<Article> articles = new ArrayList<>();
-	
+	static List<Member> members = new ArrayList<>();
+
 	public static void main(String[] args) {
 
 		System.out.println("==프로그램 시작==");
@@ -22,7 +23,6 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
-
 
 		while (true) {
 
@@ -77,15 +77,7 @@ public class Main {
 
 				int id = Integer.parseInt(cmdDiv[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
@@ -97,36 +89,8 @@ public class Main {
 				System.out.println("수정날짜 : " + foundArticle.updateDate);
 				System.out.println("제목 : " + foundArticle.title);
 				System.out.println("내용 : " + foundArticle.content);
+				System.out.println("조회 수 : " + foundArticle.hit);
 				foundArticle.hit++;
-
-				/* 게시글 삭제 */
-			} else if (command.startsWith("article delete")) {
-				String[] cmdDiv = command.split(" ");
-
-				if (cmdDiv.length < 3) {
-					System.out.println("명령어를 확인해주세요");
-					continue;
-				}
-
-				int id = Integer.parseInt(cmdDiv[2]);
-
-//				Article foundArticle = null; '없다'를 가정
-				int foundIndex = -1; // '없다'를 가정
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.id == id) {
-//						foundArticle = article;
-						foundIndex = i;
-						break;
-					}
-				}
-				if (foundIndex == -1) { // foundArticle == null;
-					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
-					continue;
-				}
-				articles.remove(foundIndex);
-				System.out.printf("%d번 게시물이 삭제되었습니다\n", id);
 
 				/* 게시글 수정 */
 			} else if (command.startsWith("article update")) {
@@ -139,15 +103,8 @@ public class Main {
 
 				int id = Integer.parseInt(cmdDiv[2]);
 
-				Article foundArticle = null;
+				Article foundArticle = getArticleById(id);
 
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 					continue;
@@ -159,7 +116,57 @@ public class Main {
 				foundArticle.content = sc.nextLine();
 				System.out.println(id + "번 게시글이 수정되었습니다, " + Util.getNowDateTimeStr());
 
-				/* 명령어 잘못 입력 */
+				/* 게시글 삭제 */
+			} else if (command.startsWith("article delete")) {
+				String[] cmdDiv = command.split(" ");
+
+				if (cmdDiv.length < 3) {
+					System.out.println("명령어를 확인해주세요");
+					continue;
+				}
+
+				int id = Integer.parseInt(cmdDiv[2]);
+
+				int foundIndex = getArticleIndexByid(id);
+
+				if (foundIndex == -1) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				articles.remove(foundIndex);
+				System.out.printf("%d번 게시물이 삭제되었습니다\n", id);
+				
+				/* 회원가입 기능 */
+			} else if (command.equals("member join")) {
+				String userID = null;
+				while(true) {
+					System.out.print("아이디 : ");
+					userID = sc.nextLine();
+					if(isJoinalbeUserID(userID) == false) {
+						System.out.println("사용 중인 아이디입니다");
+						continue; // false == false > 사용 불가능해 == 불가능 > true > 다시 입력
+					}
+					System.out.println("멋진 아이디입니다");
+					break;
+				}
+				System.out.print("비밀번호 : ");
+				String password = sc.nextLine();
+				System.out.print("비밀번호 확인 : ");
+				String passwordConfirm = sc.nextLine();
+				System.out.print("이름 : ");
+				String name = sc.nextLine();
+				
+				Member member = new Member(userID, password, name);
+				members.add(member);
+				
+			/* 명령어 잘못 입력 */
+			} else if(command.equals("member list")) {
+				for (Member member : members) {
+					System.out.println(member.userID);
+					System.out.println(member.userPassword);
+					System.out.println(member.userName);
+				}
+				
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
@@ -169,7 +176,47 @@ public class Main {
 
 		sc.close();
 	}
+
+	public static boolean isJoinalbeUserID(String userID) {
+		int index = getMemberByUserID(userID);
+		if(index == -1) {
+			return true; // 일치하는 값이 없었다 > 그러니 사용 가능하다
+		}
+		return false; // 일치하는 값이 있었다 > 중복된다
+	}
 	
+	public static int getMemberByUserID(String userID) {
+		int i = 0;
+		for (Member member : members) {
+			if (member.userID.equals(userID)) { // equals와 ==의 차이
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+
+	public static int getArticleIndexByid(int id) {
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+			if (article.id == id) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static Article getArticleById(int id) {
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+			if (article.id == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+
+	/* 테스트 데이터 생성 */
 	public static void makeTestData() {
 		System.out.println("테스트를 위한 데이터를 생성합니다");
 		articles.add(new Article(1, Util.getNowDateTimeStr(), Util.getNowDateTimeStr(), "제목1", "내용1", 11));
@@ -188,14 +235,10 @@ class Article {
 	String time;
 	int hit;
 
-	Article() {
-
-	}
-
 	Article(int id, String regDate, String updateDate, String title, String content) {
 		this(id, regDate, updateDate, title, content, 0);
 	}
-	
+
 	Article(int id, String regDate, String updateDate, String title, String content, int hit) {
 		this.id = id;
 		this.regDate = regDate;
@@ -203,5 +246,18 @@ class Article {
 		this.title = title;
 		this.content = content;
 		this.hit = hit;
+	}
+}
+
+class Member {
+	int id;
+	String userID;
+	String userPassword;
+	String userName;
+	
+	Member(String userID, String userPassword, String userName) {
+		this.userID = userID;
+		this.userPassword =  userPassword;
+		this.userName = userName;
 	}
 }
